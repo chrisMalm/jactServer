3ecf9f3ab03ebe2dbce8cc9b1820a57f7184898e
Christoffer Malmgren <christoffer.malmgren@ericsson.com>
2021-06-09 23:59:06 +0200

    added post method for twitter feed

--- server/index.js
+++ server/index.js
@@ -10,12 +10,21 @@ app.use(cors());
 
 const streamURL = 'https://api.twitter.com/2/users/2359834170/tweets';
 
+let jactTweets = [];
+
 needle.get(
   streamURL,
   { headers: { Authorization: `Bearer ${TOKEN}` } },
   (error, res) => {
-    if (!error && res.statusCode == 200) console.log(res.body);
+    if (!error && res.statusCode == 200) {
+      jactTweets.push(res.body);
+      console.log(jactTweets);
+    }
   }
 );
+
+app.post('/', function (req, res) {
+  res.send(jactTweets);
+});
 
 app.listen(PORT, () => console.log(`listning on port ${PORT}`));
